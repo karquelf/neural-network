3275c0f2b61788b9737b0f78d5f2c70aaacadd1c
Fabien Karquel <fabien@karquel.com>
2023-06-25 22:54:21 +0200

    feat: activate neurons and make guesses

--- app.rb
+++ app.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
-# Usage: ruby app.rb train|run
+# Usage: ruby app.rb train
+
+require 'benchmark'
+require 'colorize'
 
 require_relative 'src/neural_network'
 
 case ARGV[0]
 when 'train'
-  NeuralNetwork.new.train
-when 'run'
-  NeuralNetwork.new.run
+  neural_network = NeuralNetwork.new
+
+  puts '...Start training...'.blue
+  time = Benchmark.measure do
+    neural_network.train
+  end
+  puts "\n...Training finished in #{time.real.round(2)} seconds...".blue
+
+  puts '...Start running...'.blue
+  time = Benchmark.measure do
+    neural_network.run
+  end
+  puts "\n...Running finished in #{time.real.round(2)} seconds...".blue
+
+  neural_network.display_results
 else
-  puts 'Usage: ruby app.rb train|run'
+  puts 'Usage: ruby app.rb train'
 end
--- src/neural_network.rb
+++ src/neural_network.rb
@@ -1,32 +1,127 @@
 # frozen_string_literal: true
 
-require 'benchmark'
+require 'byebug'
 require 'chunky_png'
 require 'colorize'
+require 'securerandom'
 
 require_relative 'data_loader'
 
 class NeuralNetwork
+  attr_reader :id, :cost_average, :success_rate
 
-  def initialize
+  LAYERS_DESIGN = [784, 16, 16, 10].freeze
+  MIN_BIAS = 0
+  MAX_BIAS = 10
+  MIN_WEIGHT = -1.0
+  MAX_WEIGHT = 1.0
+
+  def initialize(id: nil)
     @data_loader = DataLoader.new
+    @id = id
+    @cost_average = 0.0
+    @success_rate = 0.0
+
+    if @id
+      # TODO: Load weights and biases from file
+    else
+      @layers = generate_new_neurons_with_random_weights_and_biases
+    end
   end
 
   def train
-    puts '...Start training...'.blue
-    time = Benchmark.measure do
-      @data_loader.each_label_with_image(kind: :training) do |label, image, i|
-        print "\r#{i + 1} / #{@data_loader.data_size} images".light_magenta
-      end
+    puts "Network #{@id} already trained.".red and return unless id.nil?
+
+    @data_loader.each_label_with_image(kind: :training) do |label, image, i|
+      activate_neurons(image)
+      cost = cost_function(label.to_i, @layers.last[:neurons])
+      @cost_average = (cost + (@cost_average * i)) / (i + 1)
+      print "\r#{i + 1} / #{@data_loader.data_size} images".light_magenta
     end
-    puts "\n...Training finished in #{time.real.round(2)} seconds...".blue
+  end
+
+  def save
+    puts 'Saving...'
   end
 
   def run
-    puts 'Running...'
+    rights_guesses = 0
+    @data_loader.each_label_with_image(kind: :run) do |label, image, i|
+      activate_neurons(image)
+      guess = guessed(@layers.last[:neurons])
+      if guess == label.to_i
+        rights_guesses += 1
+      else
+        generate_png("guessed #{guess} - #{label} - #{SecureRandom.uuid}", image)
+      end
+      print "\r#{i + 1} / #{@data_loader.data_size} images".light_magenta
+    end
+    @success_rate = (rights_guesses / @data_loader.data_size.to_f) * 100
+  end
+
+  def display_results
+    puts 'Results:'
+    puts "Success rate: #{@success_rate}%".green
   end
 
   private
+
+  def generate_new_neurons_with_random_weights_and_biases
+    LAYERS_DESIGN.each_with_index.map do |neuron_count, index|
+      neurons = Array.new(neuron_count, 0)
+      bias = index.positive? ? rand(MIN_BIAS..MAX_BIAS) : nil
+      weights = neuron_count.times.map do
+        rand(MIN_WEIGHT..MAX_WEIGHT)
+      end
+      {
+        neurons:,
+        bias:,
+        weights:
+      }
+    end
+  end
+
+  def activate_neurons(image)
+    @layers.each_with_index do |layer, layer_index|
+      if layer_index.zero?
+        layer[:neurons] = image.flatten.map { |pixel| pixel / 255.0 }
+      else
+        layer[:neurons] = layer[:neurons].size.times.map do |neuron_index|
+          activate_neuron(
+            @layers[layer_index - 1][:neurons],
+            @layers[layer_index - 1][:weights],
+            layer[:bias][neuron_index]
+          )
+        end
+      end
+    end
+  end
+
+  def activate_neuron(inputs, weights, bias)
+    sum = bias
+    weights.each_with_index do |weight, i|
+      sum += weight * inputs[i].to_f
+    end
+    @value = sigmoid(sum)
+  end
+
+  def sigmoid(x)
+    1 / (1 + Math.exp(-x))
+  end
+
+  def cost_function(expected, output)
+    output.map.with_index do |value, index|
+      if index + 1 == expected
+        (value - 1)**2
+      else
+        value**2
+      end
+    end.sum
+  end
+
+  def guessed(output)
+    output.index(output.max) + 1
+  end
 
   def generate_png(label, image)
     png = ChunkyPNG::Image.new(image.first.size, image.size, ChunkyPNG::Color::TRANSPARENT)
@@ -37,5 +132,4 @@ class NeuralNetwork
     end
     png.save("tmp/#{label}.png", interlace: true)
   end
-
 end
